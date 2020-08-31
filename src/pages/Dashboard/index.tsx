027e571e85bede1c614e63ffb2349ae71c313d16
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-31 18:01:09 -0300

    Estilização do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import {FiChevronRight} from 'react-icons/fi';
 
-import {Title} from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import {Title, Form, Repositories} from './styles';
 
 const Dashboard: React.FunctionComponent = () => {
-  return <Title>Explore repositórios no Github</Title>;
-}
+  return (
+  <>
+    <img src={logoImg} alt="Github Explorer"/>
+    <Title>Explore repositórios no Github</Title>
+
+    <Form >
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img src="https://avatars0.githubusercontent.com/u/33937166?s=460&u=26a01f6748c2d7594cd999eca5ba640b0972d72c&v=4" alt="Renato Lôbo" />
+     
+        <div>
+          <strong> repository/git/tal</strong>
+          <p>Descrição do repository</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+      </a>
+
+      <a href="teste">
+        <img src="https://avatars0.githubusercontent.com/u/33937166?s=460&u=26a01f6748c2d7594cd999eca5ba640b0972d72c&v=4" alt="Renato Lôbo" />
+     
+        <div>
+          <strong> repository/git/tal</strong>
+          <p>Descrição do repository</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+      </a>
+
+      <a href="teste">
+        <img src="https://avatars0.githubusercontent.com/u/33937166?s=460&u=26a01f6748c2d7594cd999eca5ba640b0972d72c&v=4" alt="Renato Lôbo" />
+     
+        <div>
+          <strong> repository/git/tal</strong>
+          <p>Descrição do repository</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+      </a>
+
+      <a href="teste">
+        <img src="https://avatars0.githubusercontent.com/u/33937166?s=460&u=26a01f6748c2d7594cd999eca5ba640b0972d72c&v=4" alt="Renato Lôbo" />
+     
+        <div>
+          <strong> repository/git/tal</strong>
+          <p>Descrição do repository</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+      </a>
+    </Repositories>
+  </>
+);}
 
 export default Dashboard;
