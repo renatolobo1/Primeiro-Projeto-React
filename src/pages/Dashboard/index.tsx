d87979247ed9be57931ab195e0c28ed00dd324f4
Renatolobo1 <renatolobo_@hotmail.com>
2020-09-01 16:17:49 -0300

    Estilizando Detalhe

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -81,16 +81,16 @@ const Dashboard: React.FunctionComponent = () => {
     <Repositories>
       { repositories.map(repository=> (
         <Link key={ repository.full_name } to={`/repositories/${repository.full_name}`}>
-        <img src={repository.owner.avatar_url} 
-        alt={repository.owner.login} />
-     
-        <div>
-          <strong> {repository.full_name}</strong>
-          <p>{repository.description}</p>
-        </div>
+          <img src={repository.owner.avatar_url} 
+          alt={repository.owner.login} />
+      
+          <div>
+            <strong> {repository.full_name}</strong>
+            <p>{repository.description}</p>
+          </div>
 
-        <FiChevronRight size={20}/>
-      </Link>
+          <FiChevronRight size={20}/>
+       </Link>
       ))}
 
       
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {useRouteMatch} from 'react-router-dom';
+import {useRouteMatch, Link} from 'react-router-dom';
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,50 @@ interface RepositoryParams {
 const Repository: React.FunctionComponent = () => {
   const {params} = useRouteMatch<RepositoryParams>();
 
-return <h1>Repository:{params.repository}</h1>;
-}
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explrorer" />
+        <Link to="/">
+          <FiChevronLeft size={16}/>
+          Voltar
+        </Link>
+      </Header> 
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars0.githubusercontent.com/u/11760617?s=460&u=47cd33f9c3dde468081dca7e6a6e8c3c54667191&v=4" alt="Rubem"/>
+          <div>
+           <strong>Rubens/algo</strong> 
+           <p>Descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo> 
+
+      <Issues>
+        <Link to="dasdasd">           
+          <div>
+            <strong>"dasdasd"</strong>
+            <p>"dasdasd"</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </Link>
+      </Issues>
+    </>
+  );
+};
 
 export default Repository;
